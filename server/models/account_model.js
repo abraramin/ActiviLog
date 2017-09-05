8151c111bc47acd4b4d8c977ff3deebf832a99e7
Perth155 <abrar.a.amin@gmail.com>
2017-09-05 17:46:19 +0800

    Account model added.

---
+++ server/models/account_model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const passportLocalValidator = require('passport-local-mongoose');
+
+var Schema = mongoose.Schema;
+
+var accountSchema = new Schema
+({
+    username: {
+      type: String,
+      require:true,
+    },
+
+    email: {
+      type: String,
+      require:true,
+      validate: {
+        validator: function(v){
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+        },
+        message: '{VALUE} is not a valid email address.'
+      },
+    },
+
+    first_name: {
+      type: String,
+      require:true,
+    },
+
+    last_name: {
+      type: String,
+      require: true,
+    },
+
+    password:{
+      type:String,
+      require:true,
+    },
+
+    organisation:{
+      type:String,
+      require: true,
+    },
+
+    date_joined:{
+      type:Date,
+      require: true,
+    }
+});
+
+accountSchema.plugin(passportLocalValidator);
+accountSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Account', accountSchema);
