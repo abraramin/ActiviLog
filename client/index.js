a82a61639bf619b56a0d84e2e59970885ddef183
Peter Joseph <contact@peterjoseph.net>
2017-09-15 10:31:17 +0800

    Renamed component folder

--- client/index.js
+++ client/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import { ACCOUNT_TYPE } from "./common/config"
-import AuthenticatedRoute from "./components/AuthenticatedRoute"
+import AuthenticatedRoute from "./pages/AuthenticatedRoute"
 
 // Load our components
-import Dashboard from './components/dashboard/';
-import Login from './components/login/';
-import Register from './components/register/';
-import Publish from './components/publish/';
-import Activites from './components/activities/';
-import Users from './components/users/';
-import MissingPath from './components/MissingPath';
+import Dashboard from './pages/dashboard/';
+import Login from './pages/login/';
+import Register from './pages/register/';
+import Publish from './pages/publish/';
+import Activites from './pages/activities/';
+import Users from './pages/users/';
+import MissingPath from './pages/MissingPath';
 
 ReactDOM.render(
 	<BrowserRouter>
