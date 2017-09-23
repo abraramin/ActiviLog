641b541ec858925a2fb43717e205c25549e1f78e
Peter Joseph <contact@peterjoseph.net>
2017-09-23 17:01:11 +0800

    Cleaned up server.js file

--- server/router.js
+++ server/router.js
@@ -1,23 +1,29 @@
 var path = require('path');
 var express = require('express');
 var passport = require('passport');
-var Account = require('../models/account');
+var Account = require('./models/account');
 var router = express.Router();
+
+
+// Logged In User Function
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    // if they aren't redirect them to the home page
+    res.status(400).send("Not Logged In!");
+}
+
 
 router.get('/api/status', function(req, res){
     res.status(200).send("Running!");
 });
 
-router.get('/api/login', function(req, res) {
-    res.render('login', { user : req.user });
-});
-
 router.post('/api/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
-});
-
-router.get('/api/register', function(req, res) {
-    res.render('register', { });
+    res.json({user: req.user });
 });
 
 router.post('/api/register', function(req, res) {
@@ -27,18 +33,26 @@ router.post('/api/register', function(req, res) {
         };
 
         passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
+            res.json({user: req.user });
         });
     });
 });
 
 router.get('/api/logout', function(req, res) {
     req.logout();
-    res.redirect('/');
+    res.status(200).send("Logged Out!");
+});
+
+router.post('/api/profile', isLoggedIn, function(req, res) {
+    if(err) {
+        res.status(400).send("failure! " + err);
+    };
+
+    res.status(200).send("Here is the profile data");
 });
 
 router.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 module.exports = router;
