40dab588fa9c320758e13ff73751bd2b033a3301
Perth155 <abrar.a.amin@gmail.com>
2017-10-06 20:23:47 +0800

    header added

--- client/common/components/Header.js
+++ client/common/components/Header.js
@@ -3,26 +3,78 @@ import PropTypes from "prop-types";
 
 import { ACCOUNT_TYPE } from "../config";
 
-require('../styles/style.css');
+require('../styles/header.css');
 
 class Header extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.activities = this.activities.bind(this);
+		this.records = this.records.bind(this);
+		this.users = this.users.bind(this);
+	}
+
+activities() {
+	return <Redirect to='/activities'/>; //URL to view activities as admin
+}
+
+records () {
+	return <Redirect to='/records'/>; //URL to view records as admin
+}
+
+users () {
+	return <Redirect to='/users'/>; //URL to view user accounts as admin
+}
+
 	render() {
 		const {
 			user,
+			logout
 		} = this.props;
 
 		if (user.loggedIn == false || user.userType == ACCOUNT_TYPE.UNREGISTERED || user.userType == null) {
 			return null;
 		}
 
-		return <div id={"header"}>
-			Header Object
-		</div>;
+		const isAdmin = user.userType == ACCOUNT_TYPE.ADMINISTRATOR;
+		const isSupervisor = user.userType == ACCOUNT_TYPE.SUPERVISOR;
+		const isUser = user.userType == ACCOUNT_TYPE.USER;
+
+		if(isUser)
+		{
+			return <div id={"header"}>
+			<div>
+				<ul>
+					<li><a>Activilog</a></li>
+					<li><a>Home</a></li>
+					<li><a>Add</a></li>
+					<li id={"logout-button"}><a onClick={this.props.logout}>Logout</a></li>
+				</ul>
+			</div>
+			</div>;
+		}
+		if(isAdmin || isSupervisor)
+		{
+			return <div id={"header"}>
+			<div>
+				<ul>
+					<li><a>Activilog</a></li>
+					<li><a>Home</a></li>
+					<li><a>Record</a></li>
+					<li><a>Activities</a></li>
+					<li><a>Accounts</a></li>
+					<li id={"logout-button"}><a onClick={this.props.logout}>Logout</a></li>
+				</ul>
+			</div>
+			</div>;
+		}
 	};
 };
 
+
 Header.propTypes = {
 	user: PropTypes.object,
+	logout: PropTypes.function
 };
 
 export default Header;
